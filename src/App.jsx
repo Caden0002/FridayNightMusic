6329d092848f0182c3b72e6d2430b4970698f8ce
OmeletteX <chen.qixuan97@gmail.com>
2024-07-31 13:39:46 +0800

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import Page1 from "./components/Page1.jsx";
-import Page2 from "./components/Page2.jsx";
 
 
 export default function App() {
     return (
         <>
             <Page1 />
-            <Page2 />
 
 
         </>
--- src/components/Page1.jsx
+++ src/components/Page1.jsx
@@ -1,11 +1,27 @@
-
+import { useState } from 'react';
+import Yamiyo from '/Yamiyo.mp3';
+import Background from '/BackgroundVideo.mp4';
+import AudioPlayer from './AudioPlayer.jsx';
 
 function Page1(props) {
     return (
-        <div className={`relative min-h-screen flex bg-stone-200`}>
-            <div className="container max-w-screen-xl mx-auto flex justify-center items-center text-4xl ">
-                Page 1
-            </div>
+        <div className={`relative min-h-screen flex`}>
+            {/* Background Video */}
+            <video
+                autoPlay
+                muted
+                loop
+                src={Background}
+                className="absolute inset-0 w-full h-full object-cover"
+                style={{ zIndex: -1 }}
+            />
+
+            {/* Audio Player */}
+            <AudioPlayer
+                audioSrc={Yamiyo}
+                songTitle='Yamiyo Lo-fi'
+                songArtist='Kijugo'
+            />
         </div>
     );
 }
