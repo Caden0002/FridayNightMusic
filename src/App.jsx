0bfddf7e6f9cdde8aaf084af8e66e0c24066a286
OmeletteX <chen.qixuan97@gmail.com>
2024-08-02 15:12:29 +0800

    “added_ga”

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,25 @@
-import React from 'react';
-import HeroStudy from "./components/HeroStudy.jsx";
-import HeroFirstNight from "./components/HeroFirstNight.jsx";
-import HeroWork from "./components/HeroWork.jsx";
-
+import React, { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import HeroStudy from './components/HeroStudy.jsx';
+import HeroFirstNight from './components/HeroFirstNight.jsx';
+import HeroWork from './components/HeroWork.jsx';
+import ReactGA from 'react-ga4';
 
 export default function App() {
-const hostname = window.location.hostname;
-const port = window.location.port;
-    return (
-   <>
-        {hostname === 'localhost' && port === '5173' && <HeroStudy />}
-        {hostname === 'study.musicplayer.cloud' && <HeroStudy />}
-        {hostname === 'firstnight.musicplayer.cloud' && <HeroFirstNight />}
-        {hostname === 'work.musicplayer.cloud' && <HeroWork />}
+  const location = useLocation();
+  const hostname = window.location.hostname;
+  const port = window.location.port;
 
+  useEffect(() => {
+    ReactGA.send({ hitType: 'pageview', page: location.pathname });
+  }, [location]);
+
+  return (
+    <>
+      {hostname === 'localhost' && port === '5173' && <HeroStudy />}
+      {hostname === 'study.musicplayer.cloud' && <HeroStudy />}
+      {hostname === 'firstnight.musicplayer.cloud' && <HeroFirstNight />}
+      {hostname === 'work.musicplayer.cloud' && <HeroWork />}
     </>
-    );
+  );
 }
