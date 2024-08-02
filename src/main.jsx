1439e8f36e2326f648701372496b4b3d06857874
OmeletteX <chen.qixuan97@gmail.com>
2024-08-02 15:07:25 +0800

    “added_ga”

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import ReactGA from 'react-ga4';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+// Initialize Google Analytics
+ReactGA.initialize('G-KRZV2Z8TEF'); 
+
+// Track page view
+ReactGA.send({ hitType: 'pageview', page: window.location.pathname });
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
