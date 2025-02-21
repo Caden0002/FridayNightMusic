5969765421f00abace674f082d2f97da1eafc961
Caden0002 <cadencqx@gmail.com>
2025-02-21 23:08:31 +0800

    update hscreen phone

--- src/components/HeroStudy.jsx
+++ src/components/HeroStudy.jsx
@@ -1,56 +1,56 @@
-import { useState, useEffect } from 'react';
-import AudioPlayer from './AudioPlayer.jsx';
-import Note from './Note.jsx';
-import Menu from './Menu.jsx'; 
+import { useState, useEffect } from "react";
+import AudioPlayer from "./AudioPlayer.jsx";
+import Note from "./Note.jsx";
+import Menu from "./Menu.jsx";
 
 function HeroStudy(props) {
-    // Hook to get the window size
-    const [windowSize, setWindowSize] = useState({
+  // Hook to get the window size
+  const [windowSize, setWindowSize] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowSize({
         width: window.innerWidth,
         height: window.innerHeight,
-    });
+      });
+    };
 
-    useEffect(() => {
-        const handleResize = () => {
-            setWindowSize({
-                width: window.innerWidth,
-                height: window.innerHeight,
-            });
-        };
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
-        window.addEventListener('resize', handleResize);
-        return () => window.removeEventListener('resize', handleResize);
-    }, []);
+  // Determine the object position based on screen size
+  const objectPosition = windowSize.width < 540 ? "left 20%" : "center"; // Adjusted position
 
-    // Determine the object position based on screen size
-    const objectPosition = windowSize.width < 540 ? 'left 20%' : 'center'; // Adjusted position
+  return (
+    <div className="relative min-h-screen h-[100dvh] flex">
+      {/* Background GIF */}
+      <img
+        src="/Study/BackgroundStudy.gif"
+        alt="Background"
+        className="absolute inset-0 w-full h-full object-cover"
+        style={{ zIndex: -1, objectPosition: objectPosition }}
+      />
 
-    return (
-        <div className="relative h-screen flex">
-            {/* Background GIF */}
-            <img
-                src='/Study/BackgroundStudy.gif'
-                alt="Background"
-                className="absolute inset-0 w-full h-full object-cover"
-                style={{ zIndex: -1, objectPosition: objectPosition }}
-            />
+      {/* Audio Player */}
+      <AudioPlayer
+        audioSrc="/Study/AudioStudy.mp3"
+        audioTitle="Yamiyo Lo-fi"
+        audioArtist="Kijugo"
+        themeColor="#d15f50"
+        audioTheme="STUDY"
+      />
 
-            {/* Audio Player */}
-            <AudioPlayer
-                audioSrc='/Study/AudioStudy.mp3'
-                audioTitle='Yamiyo Lo-fi'
-                audioArtist='Kijugo'
-                themeColor='#d15f50'
-                audioTheme='STUDY'
-            />
+      {/* Note Component */}
+      <Note />
 
-            {/* Note Component */}
-            <Note />
-
-            {/* Menu Component */}
-            <Menu /> 
-        </div>
-    );
+      {/* Menu Component */}
+      <Menu />
+    </div>
+  );
 }
 
 export default HeroStudy;
--- src/components/HeroWork.jsx
+++ src/components/HeroWork.jsx
@@ -1,57 +1,56 @@
-import { useState, useEffect } from 'react';
-import AudioPlayer from './AudioPlayer.jsx';
-import Note from './Note.jsx';
-import Menu from './Menu.jsx'; 
-
+import { useState, useEffect } from "react";
+import AudioPlayer from "./AudioPlayer.jsx";
+import Note from "./Note.jsx";
+import Menu from "./Menu.jsx";
 
 function HeroWork(props) {
-    // Hook to get the window size
-    const [windowSize, setWindowSize] = useState({
+  // Hook to get the window size
+  const [windowSize, setWindowSize] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowSize({
         width: window.innerWidth,
         height: window.innerHeight,
-    });
+      });
+    };
 
-    useEffect(() => {
-        const handleResize = () => {
-            setWindowSize({
-                width: window.innerWidth,
-                height: window.innerHeight,
-            });
-        };
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
-        window.addEventListener('resize', handleResize);
-        return () => window.removeEventListener('resize', handleResize);
-    }, []);
+  // Determine the object position based on screen size
+  const objectPosition = windowSize.width < 540 ? "20% 0" : "left";
 
-    // Determine the object position based on screen size
-const objectPosition = windowSize.width < 540 ? '20% 0' : 'left';
+  return (
+    <div className="relative min-h-screen h-[100dvh] flex">
+      {/* Background GIF */}
+      <img
+        src="/Work/BackgroundWork.gif"
+        alt="Background"
+        className="absolute inset-0 w-full h-full object-cover"
+        style={{ zIndex: -1, objectPosition: objectPosition }}
+      />
 
-    return (
-        <div className="relative h-screen flex">
-            {/* Background GIF */}
-            <img
-                src='/Work/BackgroundWork.gif'
-                alt="Background"
-                className="absolute inset-0 w-full h-full object-cover"
-                style={{ zIndex: -1, objectPosition: objectPosition }}
-            />
+      {/* Audio Player */}
+      <AudioPlayer
+        audioSrc="/Work/AudioWork.mp3"
+        audioTitle="Show Me How"
+        audioArtist="Men I Trust"
+        themeColor="#e5d6ae"
+        audioTheme="0530PM"
+      />
 
-            {/* Audio Player */}
-            <AudioPlayer
-                audioSrc='/Work/AudioWork.mp3'
-                audioTitle='Show Me How'
-                audioArtist='Men I Trust'
-                themeColor='#e5d6ae'
-                audioTheme='0530PM'
-            />
+      {/* Note Component */}
+      <Note />
 
-            {/* Note Component */}
-            <Note />
-
-            {/* Menu Component */}
-            <Menu /> 
-        </div>
-    );
+      {/* Menu Component */}
+      <Menu />
+    </div>
+  );
 }
 
 export default HeroWork;
