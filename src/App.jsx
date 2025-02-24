a71686570ed135c349eb6a41299069f134f98746
Caden0002 <cadencqx@gmail.com>
2025-02-24 13:39:58 +0800

    add default routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import Hero from "./components/Hero.jsx"; // Import dynamic Hero component
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/:themeName" element={<Hero />} /> {/* Dynamic Route */}
+        {/* Default route redirects to /work */}
+        <Route path="/" element={<Navigate replace to="/work" />} />
+
+        {/* Dynamic Route */}
+        <Route path="/:themeName" element={<Hero />} />
       </Routes>
     </Router>
   );
